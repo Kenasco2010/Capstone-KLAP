2ddae6f96e8068a08a0a29d07c0d22fe9a336d20
Kennedy <kennedy.anyinatoe@meltwater.org>
2015-05-20 20:00:11 +0000

    the landing page has been updated with the new idea, so we can all start working on something

--- client/javascript/custom_js.js
+++ client/javascript/custom_js.js
@@ -1,3 +1,8 @@
+// Template.home.rendered = function () {
+// 	$('#this-carousel-id').carousel({
+// 		interval: 2000
+// 	});
+// }; 
 $(document).ready(function(){
 	$('.carousel').carousel({
 		interval: 2000
--- client/main.js
+++ client/main.js
@@ -1,3 +1,16 @@
 Template.registerHelper('emailscount',function(){
 	return Emails.find().count()
 })
+
+Template.home.events({
+	'click .submit': function(){
+		// var data = Emails.find().fetch()
+		// var entry = document.getElementById('emails').value;
+		// console.log(entry)
+		// if (entry === data){
+		// 	alert("you hgave entered data 2x");
+		// }
+	}
+
+})
+
