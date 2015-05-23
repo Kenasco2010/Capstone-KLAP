50de8b087a2bc6e2ad12bb136ae435a6b093e4db
niinyarko <niinyarko@yahoo.com>
2015-05-23 14:12:50 +0000

    replaced html create profile form with meteor autoform

--- client/javascript/events.js
+++ client/javascript/events.js
@@ -5,3 +5,25 @@ Template.navigation.events({
     })
 }
 });
+
+Template.createProfile.events({
+    "submit form[name='createProfileForm']": function (e,t) {
+        var first_name = $("input[name='firstname']").val();
+        var last_name = $("input[name='lastname']").val();
+        var country = $("#country").val();
+        var city = $("#city").val();
+        var month = $("#month").val();
+        var day = $("#day").val();
+        var year = $("#year").val();
+        console.log(first_name);
+        console.log(last_name);
+        console.log(country);
+        console.log(city);
+        console.log(month);
+        console.log(day);
+        console.log(year);
+        $("form[name='createProfileForm']").reset();
+        return false;
+        // e.preventDefault();
+    }
+});
