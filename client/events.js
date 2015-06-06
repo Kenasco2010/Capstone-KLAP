51b4d6bdd9fd13886c7b335dd33fb0dbd64ea3e2
Kennedy <kennedy.anyinatoe@meltwater.org>
2015-06-06 15:00:29 +0000

    country and city feature working

--- client/events.js
+++ client/events.js
@@ -1,16 +1,16 @@
 Template.navigation.events({
-    "click [data-action='sign-out']": function () {
-        Meteor.logout(function() {
-          Router.go('home');
+  "click [data-action='sign-out']": function () {
+    Meteor.logout(function() {
+      Router.go('home');
     })
-}
+  }
 });
 
 Template.myProfile.events({
   "click [data-action='delete-item']": function (e, t) {
-      var ItemId = e.currentTarget.getAttribute('data-id');
-      var item = Items.findOne(ItemId);
-      sweetAlert({
+    var ItemId = e.currentTarget.getAttribute('data-id');
+    var item = Items.findOne(ItemId);
+    sweetAlert({
       title: "Are you sure?",
       text: "You will not be able to recover this post!",
       type: "warning",
@@ -22,27 +22,27 @@ Template.myProfile.events({
     }, function(){
       Meteor.call('removeItem', ItemId, function (error, result) {
         if (error) {}
-        else {
-          S3.delete(
+          else {
+            S3.delete(
               item.relativeImageUrl,
-          function(error, success) {
-              if (error) {
+              function(error, success) {
+                if (error) {
                   console.log(error);
-              };
-          });
-        }
-      });
+                };
+              });
+          }
+        });
       swal("Deleted!",
-      "this post has been deleted.",
-      "success");
+        "this post has been deleted.",
+        "success");
     });
 
     
   },
   "click [data-action='delete-trip']": function(e,t){
-      var tripId = e.currentTarget.getAttribute('data-id');
-      var trip = Travels.findOne(tripId);
-      sweetAlert({
+    var tripId = e.currentTarget.getAttribute('data-id');
+    var trip = Travels.findOne(tripId);
+    sweetAlert({
       title: "Are you sure?",
       text: "You will not be able to recover this post!",
       type: "warning",
@@ -54,12 +54,12 @@ Template.myProfile.events({
     }, function(){
       Meteor.call('removeTrip', tripId, function (error, result) {
         if (error) {}
-        else {
-        }
-      });
+          else {
+          }
+        });
       swal("Deleted!",
-      "this post has been deleted.",
-      "success");
+        "this post has been deleted.",
+        "success");
     });
   },
   "click .list-group": function(e, t){
@@ -69,124 +69,124 @@ Template.myProfile.events({
 });
 
 Template.userPublicProfile.rendered = function () {
-    $this = this;
-    ratings = $this.ratings.get();
-    userId = $this.userId.get();
-    $('.ui.rating').rating('setting', 'onRate', function(value) {
+  $this = this;
+  ratings = $this.ratings.get();
+  userId = $this.userId.get();
+  $('.ui.rating').rating('setting', 'onRate', function(value) {
           // Session.set("review-value", value);
           $this.ratings.set(value);
           $this.userId.set(this.id);
-      });
+        });
 };
 
 Template.userPublicProfile.events({
-    'click .rating': function () {
-        console.log(this);
-       console.log(Session.get("review-value"));
-    },
-    'click #hide-reviews': function() {
-      $(".second-container").slideUp();
-    },
-    'click .show-reviews': function() {
-       $(".second-container").slideDown();
-    },
-    'click .btn-send-msg': function() {
+  'click .rating': function () {
+    console.log(this);
+    console.log(Session.get("review-value"));
+  },
+  'click #hide-reviews': function() {
+    $(".second-container").slideUp();
+  },
+  'click .show-reviews': function() {
+   $(".second-container").slideDown();
+ },
+ 'click .btn-send-msg': function() {
       // $("#messageModal").modal("show");
       $('.ui.send-message.modal')
-        .modal('show')
+      .modal('show')
       ;
     },
     'click .btn-send-request': function(){
       var carrierId = $("p.getme").attr("data-user-id");
       $(".ui.send-request.modal").modal("setting", {
-          closable: false,
-          onApprove: function () {
-             var req_carry_itemId = Session.get("itemSelected");
-            Meteor.call('requestToCarry', req_carry_itemId, carrierId, function (error, result) {
-              if (error) {
-                $('.ui.send-request.modal').modal('hide');
-                swal("Sorry! something went wrong");
-              }
-              else {
-                  $('.ui.send-request.modal').modal('hide');
-                  swal("Thanks! your request has been sent");
-              }
-            });
-            return false;
+        closable: false,
+        onApprove: function () {
+         var req_carry_itemId = Session.get("itemSelected");
+         Meteor.call('requestToCarry', req_carry_itemId, carrierId, function (error, result) {
+          if (error) {
+            $('.ui.send-request.modal').modal('hide');
+            swal("Sorry! something went wrong");
           }
-      }).modal("show");
+          else {
+            $('.ui.send-request.modal').modal('hide');
+            swal("Thanks! your request has been sent");
+          }
+        });
+         return false;
+       }
+     }).modal("show");
     }
-});
+  });
 
 
 Template.userPublicProfile.created = function () {
-    var instance = this;
+  var instance = this;
     // initialize the reactive variable
-     instance.ratings = new ReactiveVar(0);
-     instance.userId = new ReactiveVar("dummytext");
+    instance.ratings = new ReactiveVar(0);
+    instance.userId = new ReactiveVar("dummytext");
 
-     instance.autorun(function () {
+    instance.autorun(function () {
 
         // get the rating
         var ratings = instance.ratings.get();
         var userId = instance.userId.get();
 
         Meteor.call("updateRatings", userId, ratings, function(err, result){
-            if (err) {
-             console.log(err);
-            }
-            else {
-             console.log(result);
-            }
-        })
+          if (err) {
+           console.log(err);
+         }
+         else {
+           console.log(result);
+         }
+       })
 
       });
-};
+  };
 
-Template.postItem.rendered = function () {
-  $(".date-picker").pickadate();
+  Template.postItem.rendered = function () {
+    $(".date-picker").pickadate();
 
-};
+  };
 
-Template.postTravel.rendered = function () {
-  $(".date-picker").pickadate();
-};
+  Template.postTravel.rendered = function () {
+    $(".date-picker").pickadate();
+  };
 
-Template.editTripForm.rendered = function () {
-  $(".date-picker").pickadate();
-};
+  Template.editTripForm.rendered = function () {
+    $(".date-picker").pickadate();
+  };
 
-Template.searchForm.rendered = function () {
-  $(".date-picker").pickadate();
-};
+  Template.searchForm.rendered = function () {
+    $(".date-picker").pickadate();
+  };
 
-Template.editItemForm.rendered = function () {
-  $(".date-picker").pickadate();
-};
+  Template.editItemForm.rendered = function () {
+    $(".date-picker").pickadate();
+  };
 
-Template.listings.rendered = function () {
-  $('.menu .item').tab();
-  new WOW().init()
-};
+  Template.listings.rendered = function () {
+    $('.menu .item').tab();
+    new WOW().init()
+  };
 
-Template.myProfile.rendered = function () {
-  $('.menu .item').tab();
-};
+  Template.myProfile.rendered = function () {
+    $('.menu .item').tab();
+  };
 
-Template.closedRequests.rendered = function () {
+  Template.closedRequests.rendered = function () {
    $('.menu .item').tab();
-};
+ };
 
-Template.requestsHome.rendered = function () {
+ Template.requestsHome.rendered = function () {
    $('.menu .item').tab();
-};
+ };
 
-Template.notifications.rendered = function () {
+ Template.notifications.rendered = function () {
   $('.menu .item').tab();
 };
 
 Template.notificationView.rendered = function () {
-   $('.menu .item').tab();
+ $('.menu .item').tab();
 };
 /*Template.navigation.rendered = function () {
   $('.ui.dropdown')
@@ -196,8 +196,8 @@ Template.notificationView.rendered = function () {
 
 Template.messageView.rendered = function () {
   $('.menu .item').tab();
-   var messageId = Router.current().params._id;
-   Meteor.call('updateReplyMessageStatus', messageId, function (error, result) {});
+  var messageId = Router.current().params._id;
+  Meteor.call('updateReplyMessageStatus', messageId, function (error, result) {});
 };
 
 Template.messagesHome.rendered = function () {
@@ -207,14 +207,34 @@ Template.messagesHome.rendered = function () {
 
 Template.postItem.events({
   'submit #postItemForm': function () {
-     if($("input.file_bag").val() == ''){
+   if($("input.file_bag").val() == ''){
         // your error validation action
         $("div#attach-pic").addClass('has-error');
         Session.set("has-error", true);
+      }
+    },
+    'change #item_post_origin_country': function (e,t) {
+      var countries = e.currentTarget.value ? e.currentTarget.value : null
+      Session.set('countries', countries);
+
+    },
+    'change #item_post_destination_country': function (e,t) {
+      var countries = e.currentTarget.value ? e.currentTarget.value : null
+      Session.set('postitemdestinationcountries', countries);
+
     }
+
+  });
+Template.postTravel.events({
+  'change #traveller_origin_country': function(e,t){
+    var countries = e.currentTarget.value ? e.currentTarget.value : null
+    Session.set('countries', countries);
+  },
+  'change #traveller_destination_country': function(e,t){
+    var countries = e.currentTarget.value ? e.currentTarget.value : null
+    Session.set('posttraveldestinationcountries', countries);
   }
 });
-
 Template.messagesHome.events({
   "click .list-group": function(e, t){
     var messageId = e.currentTarget.getAttribute('data-id');
@@ -227,61 +247,61 @@ Template.selectItem.rendered = function () {
      .dropdown('restore default text')
      ;*/
 
-  $(".ui.selection.dropdown").dropdown({
+     $(".ui.selection.dropdown").dropdown({
       onChange: function (val) {
-          Session.set("itemSelected", val);
+        Session.set("itemSelected", val);
       }
-  })
+    })
 
-};
+   };
 
-Template.itemDetails.events({
-  'click .btn-carry-req': function (e, t) {
-    var senderId = $("p.get-item-owner").attr("data-user-id");
-    var app_carry_itemId = $("p.get-item-id").attr("data-item-id");
-    Meteor.call('applicationToCarry', app_carry_itemId, senderId, function (error, result) {
-      if (error) {
-        swal("Sorry! something went wrong");
-      }
-      else {
+   Template.itemDetails.events({
+    'click .btn-carry-req': function (e, t) {
+      var senderId = $("p.get-item-owner").attr("data-user-id");
+      var app_carry_itemId = $("p.get-item-id").attr("data-item-id");
+      Meteor.call('applicationToCarry', app_carry_itemId, senderId, function (error, result) {
+        if (error) {
+          swal("Sorry! something went wrong");
+        }
+        else {
           swal("Thanks! your request has been sent");
-      }
-    });
-  },
-  'click #send-msg': function(e, t){
-    $('.ui.send-message.modal')
+        }
+      });
+    },
+    'click #send-msg': function(e, t){
+      $('.ui.send-message.modal')
       .modal('show')
-    ;
-  }
-});
+      ;
+    }
+  });
 
-Template.requestDetails.events({
-  'click #accept-btn': function (e, t) {
-    var itemId = e.currentTarget.getAttribute('data-id');
-    var item = Items.findOne(itemId);
-    var itemOwner = item.owner;
-    var recipient = itemOwner;
-    var from = Meteor.userId();
-    var status = "accepted"
-    Meteor.call('acceptedItemStatus', status, itemId, function (error, result) {});
-    Meteor.call('sendAcceptedNotification', recipient, from, itemId, function (error, result) {});
-  },
-  'click #reject-btn': function(e, t){
-    var itemId = e.currentTarget.getAttribute('data-id');
-    var item = Items.findOne(itemId);
-    var itemOwner = item.owner;
-    var recipient = itemOwner;
-    var from = Meteor.userId();
-    var status = "rejected"
-    Meteor.call('rejectedItemStatus', status, itemId, function (error, result) {});
-    Meteor.call('sendRejectedNotification', recipient, from, itemId, function (error, result) {});
-  }
-});
+   Template.requestDetails.events({
+    'click #accept-btn': function (e, t) {
+      var itemId = e.currentTarget.getAttribute('data-id');
+      var item = Items.findOne(itemId);
+      var itemOwner = item.owner;
+      var recipient = itemOwner;
+      var from = Meteor.userId();
+      var status = "accepted"
+      Meteor.call('acceptedItemStatus', status, itemId, function (error, result) {});
+      Meteor.call('sendAcceptedNotification', recipient, from, itemId, function (error, result) {});
+    },
+    'click #reject-btn': function(e, t){
+      var itemId = e.currentTarget.getAttribute('data-id');
+      var item = Items.findOne(itemId);
+      var itemOwner = item.owner;
+      var recipient = itemOwner;
+      var from = Meteor.userId();
+      var status = "rejected"
+      Meteor.call('rejectedItemStatus', status, itemId, function (error, result) {});
+      Meteor.call('sendRejectedNotification', recipient, from, itemId, function (error, result) {});
+    }
+  });
 
-Template.notificationView.events({
-  'click .notif-send-msg': function () {
-    $('.ui.send-message.modal')
+   Template.notificationView.events({
+    'click .notif-send-msg': function () {
+      $('.ui.send-message.modal')
       .modal('show')
-    ;
-  }
-});
+      ;
+    }
+  });
