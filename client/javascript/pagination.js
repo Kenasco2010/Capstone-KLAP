c54b079ddba2837f0ee4c0f25d6e983da75182d8
niinyarko <niinyarko@yahoo.com>
2015-06-08 16:40:29 +0000

    added pagination to list pages

---
+++ client/javascript/pagination.js
@@ -0,0 +1,6 @@
+ItemsPagination = new Paginator(Items);
+TripsPagination = new Paginator(Travels);
+MessagesPagination = new Paginator(Messages);
+RequestsPagination = new Paginator(Requests);
+NotificationsPagination = new Paginator(Notifications);
+ReviewsPagination = new Paginator(Reviews);
